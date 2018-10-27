dbd7976cbc56e550a3848b35b54125af2bd26c6d
neardws <singleangel@vip.qq.com>
2018-10-27 14:25:04 +0800

    add tool

--- processData.py
+++ processData.py
@@ -1,5 +1,7 @@
 import pymysql.cursors
 
+baseid = 0
+
 # 连接数据库
 connect = pymysql.Connect(
     host='120.78.167.211',
@@ -15,9 +17,9 @@ cursor = connect.cursor()
 print("connect DB success")
 
 # SQL 查询条件
-condition = "WHERE `timeStamp`>'5645c000' AND `timeStamp`<'5645c708' " \
-          "AND latitude>'3cfa47' AND latitude<'3d052e'" \
-          "AND longitude>'b18603'AND longitude<'b1954a' GROUP BY VehicleID"
+condition = "WHERE `timeStamp`>='5645c000' AND `timeStamp`<='5645c708' " \
+          "AND latitude>='3cfa47' AND latitude<='3d052e'" \
+          "AND longitude>='b18603'AND longitude<='b1954a' GROUP BY VehicleID"
 
 
 def getvehicleid():
@@ -32,7 +34,8 @@ def timelength():
     print("query timelength success")
     for time in times:
         sum += time[0]
-    print("SUM is "+str(sum))
+        print(str(time[0]))
+    print("SUM is "+str(len(times)))
     avg = sum / len(times)
     print("AVG is "+str(avg))
     i = 0
