18a253f79159d467265cff1e4f73f0e8ef576a1c
neardws <singleangel@vip.qq.com>
2019-01-05 21:30:33 +0800

    add count

---
+++ 2019-01-02/count.py
@@ -0,0 +1,33 @@
+import re
+
+
+def count(filename):
+    arr = [0]*601
+    sum = 0
+    i = 0
+    with open(filename) as f:
+        content = f.readlines()
+    for line in content:
+        print(line)
+        pattern = re.compile(r'ns_ at [0-9]\d*')
+        match = pattern.search(line)
+        if match:
+            print(match.group())
+            numpattern = re.compile('[0-9]\d*')
+            nummatch = numpattern.search(match.group())
+            num = int(nummatch.group())
+            arr[num] = arr[num] + 1
+        else:
+            print('not match')
+    for times in arr:
+        print(times)
+        sum = sum + times
+        i = i + 1
+    avarage = sum / i
+    print('sum is '+ str(sum))
+    print('i = ' + str(i))
+    print('average is ' + str(avarage))
+
+
+count("beijing5am.tcl")
+
