d45eb22506e33e47f7493292a37663bbe689e207
neardws <singleangel@vip.qq.com>
2019-02-28 12:49:27 +0800

    addnodes

---
+++ 2019-02-27/addNode.py
@@ -0,0 +1,39 @@
+import re
+
+
+def writeline(line):
+    try:
+        tclfile = open("beijing3pmfilterspeed500addnode.tcl","a")
+        tclfile.writelines(line)
+    finally:
+
+        if tclfile:
+            tclfile.close()
+
+
+def filter(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    i = 0
+    for line in content:
+        # print(line)
+        pattern = re.compile(r'ns_ at [0-9]\d*')
+        match = pattern.search(line)
+        if match:
+            numpattern = re.compile('[0-9]\d*')
+            nummatch = numpattern.search(match.group())
+            num = int(nummatch.group())
+            if num < 480:
+                writeline(line)
+            else:
+                if num == 480:
+                    for i in range(491,600):
+                        writeline(line.replace(nummatch.group(),str(i)))
+                else:
+                    pass
+        else:
+            writeline(line)
+            # print('not match')
+
+
+filter("beijing3pmfilterspeed500.tcl")
